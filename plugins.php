325dd1a9c909f761cf9ed02a8d0dc4419852e69f
Steve Chambers <steve.chambers@viewyonder.com>
2024-02-09 17:14:36 +0000

    plugins array

---
+++ plugins.php
@@ -0,0 +1,37 @@
+<?php
+
+$plugins = array (
+    array (
+        [Name] => Hello Dolly
+        [PluginURI] => http://wordpress.org/plugins/hello-dolly/
+        [Version] => 1.7.2
+        [Description] => This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+        [Author] => Matt Mullenweg
+        [AuthorURI] => http://ma.tt/
+        [TextDomain] => hello-dolly
+        [DomainPath] => 
+        [Network] => 
+        [RequiresWP] => 
+        [RequiresPHP] => 
+        [UpdateURI] => 
+        [Title] => Hello Dolly
+        [AuthorName] => Matt Mullenweg
+    )
+
+    array (
+        [Name] => Viewyonder Plugin-Alive
+        [PluginURI] => https://github.com/viewyonder/vyond-alive
+        [Version] => 1.0.1
+        [Description] => Handle the basics with this plugin.
+        [Author] => Steve Chambers
+        [AuthorURI] => https://viewyonder.com/about/steve-chambers
+        [TextDomain] => vyond-alive
+        [DomainPath] => /languages
+        [Network] => 
+        [RequiresWP] => 
+        [RequiresPHP] => 
+        [UpdateURI] => https://github.com/viewyonder/vyond-alive
+        [Title] => Viewyonder Plugin-Alive
+        [AuthorName] => Steve Chambers
+    )
+)
